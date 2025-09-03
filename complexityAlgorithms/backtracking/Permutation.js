08071cd3fd85076c73c4f7e127bc78f836e582a4
br7forlife <diazpachecobrayan@gmail.com>
2025-09-03 10:18:18 -0500

    some changes

--- complexityAlgorithms/backtracking/Permutation.js
+++ complexityAlgorithms/backtracking/Permutation.js
@@ -1,19 +1,20 @@
+//Dado un arreglo [1,2,3], generar todas las formas posibles de ordenarlo.
+
 function permutaciones(nums) {
   let resultados = [];
 
   function backtrack(camino, disponibles) {
     if (disponibles.length === 0) {
-      resultados.push([...camino]); 
+      resultados.push([...camino]);
       return;
     }
 
     for (let i = 0; i < disponibles.length; i++) {
-
       camino.push(disponibles[i]);
 
       backtrack(
         camino,
-        disponibles.filter((_, index) => index !== i) 
+        disponibles.filter((_, index) => index !== i)
       );
 
       camino.pop();
